d5ed96f4e1c257b0100d8dd833f45e8d6fcef051
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2017-10-21 00:34:16 +1100

    Dice roll
    Dice roll

--- app.js
+++ app.js
@@ -9,18 +9,32 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice
+var scores, roundScore, activePlayer
 
 scores = [0,0]
 roundScore = 0
 activePlayer = 0
 
-dice = Math.floor(Math.random() * 6 + 1)
-
-document.querySelector('#current-' + activePlayer).textContent = dice
+//document.querySelector('#current-' + activePlayer).textContent = dice
 //document.querySelector('#current-' + activePlayer).innerHTML = '<strong>' + dice + '</strong>'
 
 var score_0 = document.querySelector('#score-0').textContent
 console.log(score_0)
 
 document.querySelector('.dice').style.display = 'none'
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6 + 1)
+
+  // 2. Display the result
+  var diceDOM = document.querySelector('.dice')
+  diceDOM.style.display = 'block'
+  diceDOM.src = 'dice-' + dice + '.png'
+  
+  // 3. Update the round score but only if rolled number was NOT a 1
+
+
+
+})
