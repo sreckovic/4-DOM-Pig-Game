42a2bfe46b056b8323d7df24b8f0ea83fe18f2aa
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2017-10-21 10:32:05 +1100

    Roll
    Roll

--- app.js
+++ app.js
@@ -15,16 +15,13 @@ scores = [0,0]
 roundScore = 0
 activePlayer = 0
 
-//document.querySelector('#current-' + activePlayer).textContent = dice
-//document.querySelector('#current-' + activePlayer).innerHTML = '<strong>' + dice + '</strong>'
-
-var score_0 = document.querySelector('#score-0').textContent
-console.log(score_0)
-
 document.querySelector('.dice').style.display = 'none'
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  
   // 1. Random number
   var dice = Math.floor(Math.random() * 6 + 1)
 
@@ -32,9 +29,23 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   var diceDOM = document.querySelector('.dice')
   diceDOM.style.display = 'block'
   diceDOM.src = 'dice-' + dice + '.png'
-  
+
   // 3. Update the round score but only if rolled number was NOT a 1
+  if (dice !== 1) {
+    // Add score
+    roundScore += dice
+    document.querySelector('#current-' + activePlayer).textContent = roundScore
+  } else {
+    // Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+    roundScore = 0
 
-
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+    
+  }
 
 })
+
+//document.querySelector('#current-' + activePlayer).textContent = dice
+//document.querySelector('#current-' + activePlayer).innerHTML = '<strong>' + dice + '</strong>'
