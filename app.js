1bdff6906e6c2b0e3df3795f741da2d10b4862d9
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2017-10-21 16:32:10 +1100

    Roll
    Roll

--- app.js
+++ app.js
@@ -37,22 +37,44 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     document.querySelector('#current-' + activePlayer).textContent = roundScore
   } else {
     // Next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-    roundScore = 0
-
-    document.getElementById('current-0').textContent = '0'
-    document.getElementById('current-1').textContent = '0'
-
-    document.querySelector('.player-0-panel').classList.toggle('active')
-    document.querySelector('.player-1-panel').classList.toggle('active')
-
-    //document.querySelector('.player-0-panel').classList.remove('active')
-    //document.querySelector('.player-1-panel').classList.add('active')
-    
-    document.querySelector('.dice').style.display = 'none'
+    nextPlayer()
   }
 
 })
 
-//document.querySelector('#current-' + activePlayer).textContent = dice
-//document.querySelector('#current-' + activePlayer).innerHTML = '<strong>' + dice + '</strong>'
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // Add current score to global score
+  scores[activePlayer] += roundScore
+
+  // Update the UI
+  document.getElementById('score-' + activePlayer).textContent = scores[activePlayer]
+
+  // Check if player won the game
+  if (scores[activePlayer] >= 20) {
+    document.getElementById('name-' + activePlayer).textContent = 'Winner!'
+    document.querySelector('.dice').style.display = 'none'
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+    
+  } else {
+    // Next player
+    nextPlayer()
+  }
+})
+
+function nextPlayer() {
+  // Next player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+  roundScore = 0
+
+  document.getElementById('current-0').textContent = '0'
+  document.getElementById('current-1').textContent = '0'
+
+  document.querySelector('.player-0-panel').classList.toggle('active')
+  document.querySelector('.player-1-panel').classList.toggle('active')
+
+  //document.querySelector('.player-0-panel').classList.remove('active')
+  //document.querySelector('.player-1-panel').classList.add('active')
+
+  document.querySelector('.dice').style.display = 'none'
+}
